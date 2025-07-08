a81c9a3715b371db8b3075fc121ba9abce22120f
R.A.REPLAN <replanbueta@gmail.com>
2025-07-08 14:00:41 +0800

    Update: database changes

--- entities/function.go
+++ entities/function.go
@@ -13,8 +13,10 @@ import (
 
 func InitializeDatabase() *sql.DB {
 	// Database connection details
-
+	// Uncomment the following line to use a local database connection
 	//connStr := "host=localhost port=5432 user=postgres password=replan dbname=replan sslmode=disable"
+
+	// Use your actual connection string here
 	connStr := "host=dpg-d1m6l83ipnbc73fs0isg-a port=5432 user=replan_user password=sh76SsHL2y75VVh6nnzhKlcUS0NSQ9Kx dbname=replan sslmode=require"
 
 	// Open database connection
@@ -49,7 +51,11 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 	username := r.FormValue("username")
 	password := r.FormValue("password") // Use `password` if plain-text; `password_hash` if already hashed
 
-	connStr := "host=localhost port=5432 user=postgres password=replan dbname=replan sslmode=disable"
+	// Uncomment the following line to use a local database connection
+	//connStr := "host=localhost port=5432 user=postgres password=replan dbname=replan sslmode=disable"
+
+	// Use your actual connection string here
+	connStr := "host=dpg-d1m6l83ipnbc73fs0isg-a port=5432 user=replan_user password=sh76SsHL2y75VVh6nnzhKlcUS0NSQ9Kx dbname=replan sslmode=require"
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		http.Error(w, "Database connection error", http.StatusInternalServerError)
